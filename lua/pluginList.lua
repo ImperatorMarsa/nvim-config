02bfe3ac694f7494cd70cd814792f89183d4f248
Timofey Sitnikov <t.sitnikov@infocom-s.ru>
2023-10-13 10:29:25 +0300

    Исправил ошибку обновления версии плагина

--- lua/pluginList.lua
+++ lua/pluginList.lua
@@ -58,29 +58,18 @@ return {
             vim.opt.list = true
             vim.opt.listchars:append "space:•"
 
-            vim.opt.termguicolors = true
-            vim.cmd [[highlight IndentBlanklineIndent1 guibg=#2d2f3d gui=nocombine]]
-            vim.cmd [[highlight IndentBlanklineIndent2 guibg=#282a36 gui=nocombine]]
-            vim.cmd [[highlight IndentBlanklineIndent3 guibg=#22242e gui=nocombine]]
-
-            require("indent_blankline").setup({
-                space_char_blankline = " ",
-                show_current_context = true,
-                show_current_context_start = true,
-
-                char = "",
-                char_highlight_list = {
-                    "IndentBlanklineIndent1",
-                    "IndentBlanklineIndent2",
-                    "IndentBlanklineIndent3",
+            local highlight = {
+                "CursorColumn",
+                "Whitespace",
+            }
+            require("ibl").setup {
+                indent = { highlight = highlight, char = "" },
+                whitespace = {
+                    highlight = highlight,
+                    remove_blankline_trail = false,
                 },
-                space_char_highlight_list = {
-                    "IndentBlanklineIndent1",
-                    "IndentBlanklineIndent2",
-                    "IndentBlanklineIndent3",
-                },
-                show_trailing_blankline_indent = false,
-            })
+                scope = { enabled = false },
+            }
         end,
     },
     -- -- --
